c39db30789f48a44458c889196bdd0eecb74cb60
smaillet <smaillet@users.noreply.github.com>
2017-09-14 23:16:26 -0700

    - Removed AppVeyor.yml as the libs can't build there (Takes too long on only 2 cores)  - updated to use HTTPS for LLVM submodule ref - moved powershell build support into a powershell script module

--- scripts/cibuild.ps1
+++
@@ -1,4 +0,0 @@
-﻿& (Join-Path $PSScriptRoot 'bootstrap.ps1')
-
-$buildScript = Join-Path $PSScriptRoot 'Build-Llvm.ps1'
-& $buildScript -Build -Platform $env:PLATFORM -Configuration $env:CONFIGURATION
--- scripts/update.ps1
+++
@@ -1,4 +0,0 @@
-﻿& (Join-Path $PSScriptRoot 'bootstrap.ps1')
-
-Write-Information "Updating submodules"
-git submodule update --init --recursive
